f9f5255f6e3971bc914f3e4f666d5364879dc60a
SuperNeon4ik <superneonn@gmail.com>
2022-06-16 19:15:50 +0300

    Added random vinebooms, lmao

--- BSAmongusSusPlugin/BSAmongusSusPluginController.cs
+++ BSAmongusSusPlugin/BSAmongusSusPluginController.cs
@@ -2,10 +2,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.Networking;
 
 namespace BSAmongusSusPlugin
 {
@@ -35,45 +37,11 @@ namespace BSAmongusSusPlugin
             GameObject.DontDestroyOnLoad(this); // Don't destroy this object on scene changes
             Instance = this;
             Plugin.Log?.Debug($"{name}: Awake()");
-        }
-        /// <summary>
-        /// Only ever called once on the first frame the script is Enabled. Start is called after any other script's Awake() and before Update().
-        /// </summary>
-        private void Start()
-        {
 
-        }
 
-        /// <summary>
-        /// Called every frame if the script is enabled.
-        /// </summary>
-        private void Update()
-        {
-
-        }
-
-        /// <summary>
-        /// Called every frame after every other enabled script's Update().
-        /// </summary>
-        private void LateUpdate()
-        {
-
-        }
-
-        /// <summary>
-        /// Called when the script becomes enabled and active
-        /// </summary>
-        private void OnEnable()
-        {
-
-        }
-
-        /// <summary>
-        /// Called when the script becomes disabled or when it is being destroyed.
-        /// </summary>
-        private void OnDisable()
-        {
-
+            // Create Random Vineboom Handler
+            var rvh = new GameObject("Random Vineboom Handler").AddComponent<RandomVineboomHandler>();
+            DontDestroyOnLoad(rvh);
         }
 
         /// <summary>
---
+++ BSAmongusSusPlugin/RandomVineboomHandler.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace BSAmongusSusPlugin
+{
+    public class RandomVineboomHandler : MonoBehaviour
+    {
+        private void Start()
+        {
+            StartCoroutine(LoadSounds());
+        }
+
+        private IEnumerator LoadSounds()
+        {
+            var folderPath = Environment.CurrentDirectory + "\\UserData\\AmongusSusPlugin";
+            if (!Directory.Exists(folderPath))
+                Directory.CreateDirectory(folderPath);
+
+            AudioClip? clip = null;
+            if (File.Exists(Path.Combine(folderPath, "vineboom.ogg")))
+            {
+                FileInfo fileInfo = new FileInfo(Path.Combine(folderPath, "vineboom.ogg"));
+                var web = GetRequest(fileInfo.FullName);
+                var operation = web.SendWebRequest();
+
+                while (!operation.isDone)
+                {
+                    yield return null;
+                }
+
+                if (web.isNetworkError || web.isHttpError)
+                {
+                    Plugin.Log.Error($"Failed to load file {name} with error {web.error}");
+                    clip = null;
+                }
+                else
+                {
+                    clip = DownloadHandlerAudioClip.GetContent(web);
+                }
+            }
+
+            yield return null;
+
+            StartCoroutine(DoRandomDelayVineboomSound(2, 180, 1, clip));
+        }
+
+        // Stolen from https://github.com/Meivyn/SoundReplacer/blob/main/SoundReplacer/SoundLoader.cs
+        private static UnityWebRequest GetRequest(string fullPath)
+        {
+            var fileUrl = "file:///" + fullPath;
+            var fileInfo = new FileInfo(fullPath);
+            var extension = fileInfo.Extension;
+            switch (extension)
+            {
+                case ".ogg":
+                    return UnityWebRequestMultimedia.GetAudioClip(fileUrl, AudioType.OGGVORBIS);
+                case ".mp3":
+                    return UnityWebRequestMultimedia.GetAudioClip(fileUrl, AudioType.MPEG);
+                case ".wav":
+                    return UnityWebRequestMultimedia.GetAudioClip(fileUrl, AudioType.WAV);
+                default:
+                    return UnityWebRequestMultimedia.GetAudioClip(fileUrl, AudioType.UNKNOWN);
+            }
+        }
+
+
+        private IEnumerator DoRandomDelayVineboomSound(int minSeconds, int maxSeconds, float randPosOffset, AudioClip? clip)
+        {
+            var go = new GameObject("Hehehehaw");
+            var source = go.AddComponent<AudioSource>();
+            source.loop = false;
+            source.playOnAwake = false;
+            source.clip = clip;
+            DontDestroyOnLoad(go);
+
+            while (true)
+            {
+                if (clip != null)
+                {
+                    go.transform.position = new Vector3(
+                        UnityEngine.Random.Range(randPosOffset * -1, randPosOffset),
+                        UnityEngine.Random.Range(randPosOffset * -1, randPosOffset),
+                        UnityEngine.Random.Range(randPosOffset * -1, randPosOffset)
+                    );
+                    source.Play();
+                }
+                yield return new WaitForSeconds(UnityEngine.Random.Range(minSeconds, maxSeconds));
+            }
+        }
+    }
+}
